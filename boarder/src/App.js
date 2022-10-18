c487c4cc2b1e5a84384b05c98166c38030ed6f1d
HeoEunJae <dmswo159@gmail.com>
2022-10-18 11:04:54 +0900

    컴포넌트 테스트완료

--- boarder/src/App.js
+++ boarder/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./Components/Main/Main";
+// import Userinfo from "./Components/Userinfo/Userinfo";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+         {/* <Route path="/" elemant={<Userinfo />} />  */}
+      </Routes>
+    </Router>
   );
 }
 
