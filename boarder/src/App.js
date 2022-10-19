4fe72ab8f286e86b3cdac6f7eb2882252f31f36e
HeoEunJae <dmswo159@gmail.com>
2022-10-19 13:19:47 +0900

    테스트테스트

--- boarder/src/App.js
+++ boarder/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./Components/Main/Main";
+// import Main from "./Components/Main/Main";
+import Login from "./Components/Login/Login";
 // import Userinfo from "./Components/Userinfo/Userinfo";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Login />} />
          {/* <Route path="/" elemant={<Userinfo />} />  */}
       </Routes>
     </Router>
---
+++ boarder/src/Components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Login.css";
+// import Button from "react-bootstrap";
+
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
